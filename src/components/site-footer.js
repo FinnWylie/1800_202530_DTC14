fda16e6ebee20931fc2e9cfd20dd0770c827551c
Rathnesh <rathneshb@gmail.com>
2025-11-01 16:01:20 -0700

    Simple review page.

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -5,7 +5,7 @@ class SiteFooter extends HTMLElement {
             <footer class="w-full py-2 px-2 border-t-4 border-black fixed bottom-0 bg-white">
                 <div class="flex justify-center items-center gap-1">
                     <!-- Home -->
-                    <button onclick="location.href='index.html'" class="flex flex-col items-center mx-auto w-[64px]">
+                    <button onclick="location.href='home_index.html'" class="flex flex-col items-center mx-auto w-[64px]">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"
                              fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M5 12l-2 0l9 -9l9 9l-2 0"/>
@@ -26,7 +26,7 @@ class SiteFooter extends HTMLElement {
                     </button>
         
                     <!-- Reviews -->
-                    <button onclick="location.href='reviews_index.html'" class="flex flex-col items-center mx-auto w-[64px]">
+                    <button onclick="location.href='review_index.html'" class="flex flex-col items-center mx-auto w-[64px]">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"
                              fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"/>
