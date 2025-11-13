6fbb7c19e6651af3f479e5ab898a07baf82760aa
Rathnesh <rathneshb@gmail.com>
2025-11-12 17:04:19 -0800

    Implemented firebase stuff for User settings menu, will add sign out functionality

--- src/app.js
+++ src/app.js
@@ -15,22 +15,27 @@ import { getDoc, doc, collection } from "https://www.gstatic.com/firebasejs/10.7
 function showDashboard() {
 
   const submenu = document.getElementById('submenu');
+  const sub = document.getElementById('sub');
+  const submenus = document.getElementById('submenus');
   onAuthReady(async (user) => {
     if (!user) {
       // If no user is signed in → redirect back to login page.
       // location.href = "index.html";
       console.log("No user logged in");
+      submenu.textContent = `No user logged in`;
       return;
     }
 
     const userDoc = await getDoc(doc(db, "users", user.uid));
     const name = userDoc.exists()
       ? userDoc.data().name
-      : user.displayName || user.email;
+      : user.displayName || user.email
 
     // Update the welcome message with their name/email.
     if (submenu) {
       submenu.textContent = `Username: ${name}!`;
+      sub.textContent = "Email: " + user.email
+      submenus.textContent = "Country: " + userDoc.data().country
       console.log(`Welcome, ${name}!`);
     }
   });
